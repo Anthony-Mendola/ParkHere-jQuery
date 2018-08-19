d1f601929269c96161fb31c6dc474291b1ece547
Anthony <github email address>
2018-08-19 15:42:56 -0400

    working on views

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,24 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+  def facebook
+    if current_user.present?
+      current_user.apply_omniauth(request.env["omniauth.auth"])
+      redirect_to edit_user_registration_path, notice: "Facebook Account Linked!"
+    else
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+
+      if @user.persisted?
+        set_flash_message(:notice, :success, :kind => "Facebook")
+        sign_in_and_redirect @user
+      else
+        session["devise.facebook_data"] = request.env["omniauth.auth"]
+        set_flash_message(:notice, :alert, :kind => "Could not sign in with Facebook")
+        redirect_to new_user_registration_url
+      end
+    end
+  end
+
+  def failure
+    redirect_to root_path
+  end
+end
